8e2c6cbc30f871b0c88dd153f8dff2449ffaf933
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-20 16:23:35 +0900

    FEAT: initialize Vue frontend and set Axios api module

---
+++ vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script>
+import apiClient from './modules/axios'
+
+export default {
+  async mounted() {
+    const data = await apiClient.get('notice');
+    console.log(data);
+  },
+  name: 'App',
+  components: {
+  }
+}
+</script>
