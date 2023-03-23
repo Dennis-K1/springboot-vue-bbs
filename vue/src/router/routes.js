44ed0b1900896f19af259a5d194e89b914303458
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-23 16:44:17 +0900

    FEAT: (front) board articleList with searchBar and pagination

--- vue/src/router/routes.js
+++ vue/src/router/routes.js
@@ -1,15 +1,26 @@
-import {defineComponent} from "vue";
 import ProfilePage from "/@/components/ProfilePage.vue";
 import HomePage from "/@/components/HomePage.vue";
+import BoardContainer from "/@/components/BoardContainer.vue";
+import NotFound from "/@/components/NotFound.vue";
 
-const NotFound = defineComponent({
-  template : `<div>Not Found</div>`
-});
+
+const boardNames = ['notice', 'inquiry', 'gallery', 'community'];
 
 const routes = [
   {path: '/', name: 'home', component: HomePage},
   {path: '/profile', name: 'profile', component: ProfilePage},
-  {path: '/:catchAll(.*)+', component: NotFound}
+  {path: '/:path',
+    name: 'board',
+    component: BoardContainer,
+    beforeEnter: (to, from, next) => {
+    const path = to.params.path;
+    //게시판명에 해당하지 않는 경우 넘김
+    if (boardNames.includes(path)) {
+      next()
+    }
+  }},
+  //  404 NotFound
+  {path: '/:catchAll(.*)*', name: 'notFound', component: NotFound}
 ]
 
 export default routes;
