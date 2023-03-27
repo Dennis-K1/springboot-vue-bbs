5793e1f1c5cae235d6114f4c4f7c709a4e8890e5
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-27 22:03:58 +0900

    feat: (front) delete comment

--- vue/src/components/boards/community/CommunityReplyList.vue
+++ vue/src/components/boards/community/CommunityReplyList.vue
@@ -11,7 +11,8 @@
       <tr>
         <td class="reply">
           <textarea v-model="replyContent" class="form-control" rows="5"></textarea>
-          <span v-if="replyContentValidationAlert === 1" class="d-flex text-danger">댓글은 1글자 이상 99글자 미만으로 작성해야 합니다.</span>
+          <span v-if="validationErrorMessage.reply"
+                class="d-flex text-danger">{{ validationErrorMessage.reply }}</span>
           <button @click="registerReply"
                   class="btn btn-primary float-end mt-2">댓글 등록
           </button>
@@ -28,23 +29,33 @@
           <span>{{ reply.dateRegistered }}</span>
         </div>
         <div v-if="reply.replyDeleted !== 1">
-            <span>{{ reply.content }}</span>
-            <button class="btn btn-primary btn-sm d-flex mb-2" @click="clickedNestedReplyButtonIndex = reply.id, nestedReplyContent=''"> 대댓글 등록 </button>
-            <div class="position-relative" v-if="clickedNestedReplyButtonIndex === reply.id">
-              <textarea v-model="nestedReplyContent" class="form-control pb-5 mb-3"></textarea>
-              <div class="position-absolute bottom-0 end-0">
-              <span v-if="nestedReplyContentValidationAlert === 1" class="text-danger me-5">댓글은 1글자 이상 99글자 미만으로 작성해야 합니다.</span>
-                <button @click="clickedNestedReplyButtonIndex = -1, nestedReplyContent='', nestedReplyContentValidationAlert = -1"
-                        class="btn btn-secondary mt-2 me-2 mb-1">취소
-                </button>
-                <button  @click="registerNestedReply(reply.id, replyIndex)"
-                         class="btn btn-primary mt-2 me-2 mb-1">등록
-                </button>
-              </div>
+          <span>{{ reply.content }}</span>
+          <div class="d-flex">
+            <button class="btn btn-primary btn-sm d-flex mb-2"
+                    @click="clickedNestedReplyButtonIndex = reply.id, nestedReplyContent='', validationErrorMessage.nestedReply=''">
+              대댓글 등록
+            </button>
+            <button class="ms-2 btn btn-danger btn-sm mb-2 d-flex" @click="deleteReply(reply.id)"
+                    v-if="reply.user.account === userLoggedIn">
+              삭제
+            </button>
+          </div>
+          <div class="position-relative" v-if="clickedNestedReplyButtonIndex === reply.id">
+            <textarea v-model="nestedReplyContent" class="form-control pb-5 mb-3"></textarea>
+            <div class="position-absolute bottom-0 end-0">
+              <span v-if="validationErrorMessage.nestedReply"
+                    class="text-danger me-2">{{ validationErrorMessage.nestedReply }}</span>
+              <button @click="clickedNestedReplyButtonIndex = -1, nestedReplyContent=''"
+                      class="btn btn-secondary mt-2 me-2 mb-1">취소
+              </button>
+              <button @click="registerNestedReply(reply.id, replyIndex)"
+                      class="btn btn-primary mt-2 me-2 mb-1">등록
+              </button>
             </div>
+          </div>
         </div>
         <div v-else>
-          <p>(숨김 처리된 댓글)
+          <p>(삭제된 댓글)
             <del>{{ reply.content }}</del>
           </p>
         </div>
@@ -57,9 +68,13 @@
             </div>
             <div v-if="nestedReply.replyDeleted != 1" class="ms-1">
               <p>{{ nestedReply.content }}</p>
+              <button class="ms-2 btn btn-danger btn-sm mb-2 d-flex" @click="deleteNestedReply(nestedReply.id, replyIndex)"
+                      v-if="reply.user.account === userLoggedIn">
+                삭제
+              </button>
             </div>
             <div v-else class="ms-1">
-              <p>(숨김 처리된 댓글)
+              <p>(삭제된 댓글)
                 <del>{{ nestedReply.content }}</del>
               </p>
             </div>
@@ -69,7 +84,8 @@
       <tr>
         <td class="reply">
           <textarea v-model="replyContent" class="form-control" rows="5"></textarea>
-          <span v-if="replyContentValidationAlert === 1" class="d-flex text-danger">댓글은 1글자 이상 99글자 미만으로 작성해야 합니다.</span>
+          <span v-if="validationErrorMessage.reply"
+                class="d-flex text-danger">{{ validationErrorMessage.reply }}</span>
           <button @click="registerReply"
                   class="btn btn-primary float-end mt-2">댓글 등록
           </button>
@@ -91,6 +107,7 @@ export default {
 
 import {inject, onMounted, ref} from "vue";
 import apiClient from "/@/modules/apiUtil.js";
+import validation from "/@/components/commons/validation.js";
 
 /**
  * ArticleDetail 에서 주입받은 댓글 목록
@@ -113,6 +130,11 @@ const replyContent = ref('');
 const nestedReplyContent = ref('');
 
 /**
+ * 로그인된 회원 아이디
+ */
+const userLoggedIn = inject('userLoggedIn');
+
+/**
  * 댓글 등록
  *
  * 1. 유효성 검사
@@ -120,13 +142,12 @@ const nestedReplyContent = ref('');
  * 3. 댓글 목록에 추가하여 반영
  */
 const registerReply = async () => {
-  if (replyContent.value === '' || replyContent.value.length < 1 || replyContent.value.length
-      > 99) {
-    replyContentValidationAlert.value = 1;
+  validationErrorMessage.value.reply = validation.validateReply(replyContent.value)
+  if (validationErrorMessage.value.reply !== '') {
     return;
   }
-  replyContentValidationAlert.value = -1;
-  let response = await apiClient.post('reply', {articleId:articleId.value, content:replyContent.value, user:{account:'asd'}});
+  let response = await apiClient.post('reply',
+      {articleId: articleId.value, content: replyContent.value, user: {account: 'asd'}});
   if (response.success) {
     replyContent.value = '';
     replyList.value.push(response.data);
@@ -141,13 +162,16 @@ const registerReply = async () => {
  * 3. 댓글 목록의 해당 대댓글 목록에 추가하여 반영
  */
 const registerNestedReply = async (replyId, replyIndex) => {
-  if (nestedReplyContent.value === '' || nestedReplyContent.value.length < 1 || nestedReplyContent.value.length
-      > 99) {
-    nestedReplyContentValidationAlert.value = 1;
+  validationErrorMessage.value.nestedReply = validation.validateReply(nestedReplyContent.value)
+  if (validationErrorMessage.value.nestedReply !== '') {
     return;
   }
-  nestedReplyContentValidationAlert.value = -1;
-  let response = await apiClient.post('reply', {articleId:articleId.value, content:nestedReplyContent.value, replyId, user:{account:'asd'}});
+  let response = await apiClient.post('reply', {
+    articleId: articleId.value,
+    content: nestedReplyContent.value,
+    replyId,
+    user: {account: 'asd'}
+  });
   if (response.success) {
     nestedReplyContent.value = '';
     replyList.value[replyIndex].nestedReplyList.push(response.data);
@@ -155,19 +179,52 @@ const registerNestedReply = async (replyId, replyIndex) => {
 }
 
 /**
+ * 유횩성 검증 에러 메세지
+ */
+const validationErrorMessage = ref({
+  reply: '',
+  nestedReply: '',
+
+})
+/**
  * 대댓글 textarea 표시를 위한 플래그
  */
-const clickedNestedReplyButtonIndex = ref(-1);
+let clickedNestedReplyButtonIndex = ref(-1);
 
 /**
- * 댓글 유효성 검사 경고를 위한 플래그
+ * 댓글 삭제
+ *
+ * @param replyId 댓글 번호
  */
-const replyContentValidationAlert = ref(-1);
+const deleteReply = async (replyId) => {
+  let response = await apiClient.delete(`reply/${replyId}`);
+  if (response.success) {
+
+    replyList.value.forEach(reply => {
+      if (reply.id === replyId) {
+        reply.replyDeleted = 1;
+      }
+    })
+  }
+}
 
 /**
- * 대댓글 유효성 검사 경고를 위한 플래그
+ * 대댓글 삭제
+ *
+ * @param nestedReplyId 대댓글 번호
+ * @param replyIndex 대댓글이 담긴 댓글 객체 인덱스
  */
-const nestedReplyContentValidationAlert = ref(-1);
+const deleteNestedReply = async (nestedReplyId, replyIndex) => {
+  let response = await apiClient.delete(`reply/${nestedReplyId}`);
+  if (response.success) {
+    let nestedReplyList = replyList.value[replyIndex].nestedReplyList;
+    nestedReplyList.forEach(reply => {
+      if (reply.id === nestedReplyId) {
+        reply.replyDeleted = 1;
+      }
+    })
+  }
+}
 
 </script>
 <style scoped>
