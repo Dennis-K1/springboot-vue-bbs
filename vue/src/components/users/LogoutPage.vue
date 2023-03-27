4c3aeae2e399110cda4816066691a1b892bcb278
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-28 00:10:55 +0900

    feat: (front) logout

---
+++ vue/src/components/users/LogoutPage.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "LogoutPage"
+}
+/**
+ * 로그아웃을 위한 빈 페이지
+ *
+ * route.js 에서 경로 진입 전에 토큰 분석 후 로그아웃 혹은 홈화면 리디렉션 실행
+ */
+</script>
+<style scoped>
+
+</style>
