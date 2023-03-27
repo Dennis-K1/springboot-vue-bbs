c5185b5eaa290b3d00b9840c62dbecf71600d4a1
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-28 01:21:53 +0900

    feat: (front) index

--- vue/src/components/HomePage.vue
+++ vue/src/components/HomePage.vue
@@ -1,75 +1,27 @@
 <template>
   <main class="container">
-    <div class="p-4 p-md-5 mb-4 text-white bg-dark mt-5">
+    <div class="p-4 p-md-5 mb-4 text-white bg-primary mt-5">
       <div class="col-md-6 px-0">
-        <h1 class="display-4 fst-italic">Title of a longer featured blog post</h1>
+        <h1 class="display-6 fst-italic">Backend : Spring Boot</h1>
+        <h1 class="display-6 fst-italic">Frontend : Vue.js</h1>
       </div>
     </div>
-
-    <div class="row mb-2">
-<!--      반복문  -->
-      <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-          <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-primary">World</strong>
-            <h3 class="mb-0">Featured post</h3>
-            <div class="mb-1 text-muted">Nov 12</div>
-            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="stretched-link">Continue reading</a>
-          </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-
+    <div class="row mt-5 mb-2">
+      <div class="mb-1 text-muted">최근 게시글 목록</div>
+      <div v-for="(articles, index) in recentArticles" :key="Object.keys(recentArticles)[index]"
+           class="col-md-6 mb-5">
+        <div class="card h-100 shadow-sm border">
+          <div class="card-body">
+            <strong class="d-inline-block mb-2 text-primary p-2">{{ boardNames[index] }}</strong>
+            <div class="ms-2" v-for="article in articles" :key="article.id">
+              <router-link :to="'/' + boardPath[index] + '/' + article.id"><h5 class="mb-0">
+                {{ article.title }}</h5>
+              </router-link>
+              <div class="mb-1 text-muted">{{ article.dateRegistered }}</div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-          <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-success">Design</strong>
-            <h3 class="mb-0">Post title</h3>
-            <div class="mb-1 text-muted">Nov 11</div>
-            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="stretched-link">Continue reading</a>
-          </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-          <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-success">Design</strong>
-            <h3 class="mb-0">Post title</h3>
-            <div class="mb-1 text-muted">Nov 11</div>
-            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="stretched-link">Continue reading</a>
-          </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-          <div class="col p-4 d-flex flex-column position-static">
-            <strong class="d-inline-block mb-2 text-success">Design</strong>
-            <h3 class="mb-0">Post title</h3>
-            <div class="mb-1 text-muted">Nov 11</div>
-            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="stretched-link">Continue reading</a>
-          </div>
-          <div class="col-auto d-none d-lg-block">
-            <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <h1 class="mt-5">Sticky footer with fixed navbar</h1>
-      <p class="lead">Pin a footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code class="small">padding-top: 60px;</code> on the <code class="small">main &gt; .container</code>.</p>
-      <p>Back to <a href="../examples/sticky-footer/">the default sticky footer</a> minus the navbar.</p>
     </div>
   </main>
 </template>
@@ -79,20 +31,78 @@ export default {
   name: "HomePage"
 }
 </script>
+<script setup>
+
+
+import apiClient from '/@/modules/apiUtil.js'
+import {computed, onMounted, reactive, ref, watchEffect} from "vue";
+
+/**
+ * 최신 게시글 목록
+ */
+const recentArticles = ref({});
+
+/**
+ * 게시판명
+ */
+const boardNames = {
+  1: "공지사항",
+  2: "자유게시판",
+  3: "1:1문의",
+  4: "갤러리"
+}
+
+/**
+ * 게시판 경로명
+ */
+const boardPath = {
+  1: "notice",
+  2: "community",
+  3: "inquiry",
+  4: "gallery"
+}
+
+/**
+ * 데이터 조회
+ */
+async function fetchData() {
+  const indexList = await apiClient.get('index');
+
+  /**
+   * 받아온 게시글을 게시판 번호에 맞추어 재구성
+   */
+  let top3RecentArticlesByEachBoard = indexList.data.top3RecentArticlesByEachBoard;
+  recentArticles.value = top3RecentArticlesByEachBoard.reduce((acc, obj) => {
+    const {boardId, ...rest} = obj;
+    if (!acc[boardId]) {
+      acc[boardId] = [];
+    }
+    acc[boardId].push(rest);
+    return acc;
+  }, {})
+}
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
 
 <style scoped>
-
 h1, h2, h3, h4, h5, h6 {
-  font-family: "Playfair Display", Georgia, "Times New Roman", serif/*rtl:Amiri, Georgia, "Times New Roman", serif*/;
+  font-family: "Playfair Display", Georgia, "Times New Roman", serif /*rtl:Amiri, Georgia, "Times New Roman", serif*/;
 }
 
 .display-4 {
   font-size: 2.5rem;
 }
+
 @media (min-width: 768px) {
   .display-4 {
     font-size: 3rem;
   }
 }
-
+a {
+  text-decoration: none;
+}
 </style>
