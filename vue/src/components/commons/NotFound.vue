c0daadce16e83611a9771ce0b2733a057c86b36d
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-28 01:23:56 +0900

    feat: (front) 404 not found

---
+++ vue/src/components/commons/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="d-flex align-items-center justify-content-center vh-100">
+    <div class="text-center">
+      <h1 class="display-1 fw-bold">404</h1>
+      <p class="fs-3"> 페이지 없음 </p>
+      <p class="lead">
+        요청하신 페이지가 존재하지 않습니다.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+}
+</script>
+
+<style scoped>
+
+</style>
