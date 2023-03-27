84be52316bdc679f2d9cc0c3916dafde275ff27d
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-28 01:22:58 +0900

    feat: (front) footer

--- vue/src/components/commons/FooterComponent.vue
+++ vue/src/components/commons/FooterComponent.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="footer mt-auto py-3 bg-light">
     <div class="container text-center">
-      <span class="text-muted">푸터</span>
+      <span class="text-muted">Email : dennis.k1.dev@gmail.com</span><br>
+      <span class="text-muted">Github : github.com/Dennis-K1/</span>
     </div>
   </footer>
 </template>
