ad2fb7b6916c98462cb65d34e65c1b227f884768
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-03-28 15:21:51 +0900

    refactor: LOGO url

--- vue/src/components/commons/NavBar.vue
+++ vue/src/components/commons/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand"> 포트폴리오 웹</a>
+    <a class="navbar-brand" href="/"> 포트폴리오 웹</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
         <span class="navbar-toggler-icon"></span>
       </button>
