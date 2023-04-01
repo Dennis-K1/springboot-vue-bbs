2509f4da77f2921249924f25607e9593ba45fdbf
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-04-01 23:05:00 +0900

    refactor: extract functions to separate composables

--- vue/src/modules/apiUtil.js
+++ vue/src/modules/apiUtil.js
@@ -1,4 +1,5 @@
 import {axios} from "@bundled-es-modules/axios";
+import store from "/@/modules/store.js";
 
 /**
  * api 요청 주소
@@ -16,89 +17,11 @@ const apiClient = axios.create({
  * 요청시 헤더에 jwt 등록
  */
 apiClient.interceptors.request.use(config => {
-  const jwt = localStorage.getItem('jwt');
+  const jwt = store.getters.getToken;
   if (jwt) {
     config.headers.Authorization = `Bearer ${jwt}`;
   }
   return config
 });
 
-export default {
-
-  /**
-   * GET 요청
-   *
-   * 성공 데이터 모두 커스텀 포맷 데이터 반환
-   *
-   * @param endpoint 대상 경로
-   * @param parameter 파라미터
-   * @returns {Promise<*>} 응답 데이터
-   */
-  async get(endpoint, parameter = null) {
-    try {
-      const response = await apiClient.get(endpoint, {params: parameter});
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-
-  /**
-   * POST 요청
-   *
-   * 성공 데이터 모두 커스텀 포맷 데이터 반환
-   * 로그인 요청이고 성공한 경우 localStorage 에 jwt 등록
-   *
-   * @param endpoint 대상 경로
-   * @param payload 전송 데이터
-   * @returns {Promise<*>} 응답 데이터
-   */
-  async post(endpoint, payload) {
-    try {
-      const response = await apiClient.post(endpoint, payload);
-      if (response.data.success && response.headers.authorization
-          !== undefined) {
-        const jwt = response.headers.authorization;
-        localStorage.setItem('jwt', jwt);
-      }
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-
-  /**
-   * DELETE 요청
-   *
-   * 성공 데이터 모두 커스텀 포맷 데이터 반환
-   *
-   * @param endpoint 대상 경로
-   * @returns {Promise<*>} 응답 데이터
-   */
-  async delete(endpoint) {
-    try {
-      const response = await apiClient.delete(endpoint);
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-
-  /**
-   * PUT 요청
-   *
-   * 성공 데이터 모두 커스텀 포맷 데이터 반환
-   *
-   * @param endpoint 대상 경로
-   * @param payload 전송 데이터
-   * @returns {Promise<*>} 응답 데이터
-   */
-  async put(endpoint, payload) {
-    try {
-      const response = await apiClient.put(endpoint, payload);
-      return response.data;
-    } catch (error) {
-      return error.response.data;
-    }
-  },
-}
+export default apiClient;
