48d08cdf571d61c4e57f5c94d18c9335f028dd71
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-04-02 00:22:19 +0900

    feat: add vuex store

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.esm.min'
 import {router} from "/@/router/router.js";
+import store from "/@/modules/store.js";
+
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 app.mount('#app');
