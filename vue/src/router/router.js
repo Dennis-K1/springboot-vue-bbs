fa3874493086a8934da66a6e0b1f4c7e4914b3e4
Dennis-K1 <dennis.k1.dev@gmail.com>
2023-04-02 00:21:50 +0900

    refactor: extract beforeEnter in routes to router as beforeEach

--- vue/src/router/router.js
+++ vue/src/router/router.js
@@ -1,8 +1,75 @@
 import {createRouter, createWebHistory} from "vue-router";
 import routes from "/@/router/routes.js";
+import store from "/@/modules/store.js";
 
 export const router = createRouter({
   history: createWebHistory(),
   linkActiveClass: 'active',
   routes
 });
+
+/**
+ * 게시판 해당 경로
+ */
+const boardPaths = ['notice', 'inquiry', 'gallery', 'community'];
+
+/**
+ * 입력폼 가능 게시판 경로
+ */
+const formPaths = ['community', 'inquiry'];
+
+/**
+ * 로그인 전 표시 가능 메뉴
+ */
+const beforeLogin = ['/signup', '/login'];
+
+/**
+ * 로그인 후 가능 메뉴
+ */
+const afterLogin = ['/profile', '/logout']
+
+router.beforeEach(async (to, from, next) => {
+
+  /**
+   * 전체 경로
+   */
+  const toPath = to.path;
+
+  /**
+   * 첫 번째 경로
+   */
+  const path = to.params.path;
+
+  /**
+   * 사용자 로그인 시 발행되는 jwt
+   */
+  const jwt = store.state.token;
+
+  /**
+   * 로그인 하지 않은 채로 로그인 후 가능 메뉴에 접근 할 시
+   */
+  if (jwt === null && afterLogin.includes(toPath)) {
+    alert('로그인 후 이용할 수 있습니다.');
+    next(`/login?redirectURL=${toPath}`);
+    return;
+  }
+
+  /**
+   * 로그인 후 로그인 전 가능 메뉴에 접근 할 시
+   */
+  if (jwt !== null && beforeLogin.includes(toPath)) {
+    next('/');
+    return;
+  }
+
+  /**
+   * 입력 가능 게시판이 아닌 게시판 경로로 접근 시
+   */
+  if (boardPaths.includes(path)) {
+    if (toPath.split('/')[2] === 'form' && !formPaths.includes(toPath.split('/')[1])) {
+      next(path);
+      return;
+    }
+  }
+  next();
+})
--- vue/src/router/routes.js
+++ vue/src/router/routes.js
@@ -7,26 +7,12 @@ import ArticleInputForm from "/@/components/boards/ArticleInputForm.vue";
 import ArticleList from "/@/components/boards/ArticleList.vue";
 import LoginPage from "/@/components/users/LoginPage.vue";
 import SignupPage from "/@/components/users/SignupPage.vue";
-import LogoutPage from "/@/components/users/LogoutPage.vue";
-
-import apiClient from "/@/modules/apiUtil.js";
-
-const boardNames = ['notice', 'inquiry', 'gallery', 'community'];
 
 const routes = [
   {
     path: '/:path',
     name: 'board',
     component: BoardContainer,
-    /**
-     * 게시판명에 해당하는 경우만 넘김
-     */
-    beforeEnter: (to, from, next) => {
-      const path = to.params.path;
-      if (boardNames.includes(path)) {
-        next()
-      }
-    },
     children: [
       {
         path: '',
@@ -39,80 +25,17 @@ const routes = [
       {
         path: 'form',
         component: ArticleInputForm,
-        /**
-         * 게시판명에 해당하는 경우만 넘김
-         */
-        beforeEnter: (to, from, next) => {
-          const path = to.params.path;
-          if (path === 'community' || path === 'inquiry') {
-            next()
-          } else {
-            next(false)
-          }
-        }
       },
     ],
   },
-
   {
     path: '/profile', name: 'profile', component: ProfilePage,
-    /**
-     * 비로그인 회원이 경로로 진입하지 못하게 처리
-     */
-    beforeEnter: (to, from, next) => {
-      const jwt = localStorage.getItem('jwt');
-      if (jwt === null) {
-        alert('로그인이 필요합니다.')
-        location.replace('/');
-      }
-      next()
-    }
   },
-  {path: '/signup', name: 'signup', component: SignupPage,
-    /**
-     * 로그인한 회원이라면 회원가입 페이지 진입 불가
-     */
-    beforeEnter: (to, from, next) => {
-      const jwt = localStorage.getItem('jwt');
-      if (jwt !== null) {
-        location.replace(from.path);
-      }
-      next()
-    }},
-  {
-    path: '/logout', name: 'logout', component: LogoutPage,
-    /**
-     * 로그아웃 경로 진입 전 로그아웃 처리 진행
-     */
-    beforeEnter: async (to, from, next) => {
-      const jwt = localStorage.getItem('jwt');
-      if (jwt === null) {
-        location.replace('/');
-      }
-      let response = await apiClient.post('users/logout', null);
-      if (response.success) {
-        localStorage.removeItem('jwt');
-        localStorage.removeItem('userLoggedIn');
-        location.replace('/');
-      }
-      next('/');
-    }
-  },
+  {path: '/signup', name: 'signup', component: SignupPage},
   {
     path: '/login', name: 'login', component: LoginPage,
-    /**
-     * 로그인 후 뒤로가기 시 로그인 전 화면으로
-     */
-    beforeEnter: (to, from, next) => {
-      const jwt = localStorage.getItem('jwt');
-      if (jwt !== null) {
-        location.replace(from.path);
-      }
-      next()
-    }
   },
   {path: '/', name: 'home', component: HomePage},
-  //  404 NotFound
   {path: '/:catchAll(.*)', name: 'notFound', component: NotFound}
 ]
 
